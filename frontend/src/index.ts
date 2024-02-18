2ae0809fb059a4dc7d9e7a0a74038a7a60716599
Konrad Zagozda <konrad.zagozda@grapeup.com>
2024-02-18 17:05:55 +0100

    scaffold frontend app

--- frontend/src/index.ts
+++
@@ -1,7 +0,0 @@
-import { PostsService } from "./client";
-
-const response = await PostsService.createPost({
-  requestBody: { content: "this is a new post!", title: "new post" },
-});
-
-response.title;
